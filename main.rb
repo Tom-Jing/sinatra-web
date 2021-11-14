fc18518054bc6d5776078b6d88764cbdec8d62b0
YuguangJing <j1ngyuguang@outlook.com>
2021-11-13 23:47:26 -0800

    v1.1.6

--- main.rb
+++ main.rb
@@ -4,15 +4,3 @@ require "dm-core"
 require "dm-migrations"
 require_relative 'routers'
 require_relative 'player'
-
-
-configure :development do
-    DataMapper.setup(:default, "sqlite3://#{Dir.pwd}/player.db")
-end
-
-configure :production do
-    DataMapper.setup(:default, ENV['DATABASE_URL'])
-end
-
-
-Player.auto_migrate!
